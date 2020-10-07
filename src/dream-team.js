3bc4d65679b8a7ce713a0d15d14446c302e8927b
Nedomorf <danikp01@,ail.ru>
2020-10-07 18:08:17 +0300

    feat: complete dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false;
+    } else {
+        let arr = [];
+        let x = 0;
+        let sp = [];
+        let firstLet = '';
+        for (let i = 0; i < members.length; i++) {
+            if (typeof (members[i]) === "string") {
+                sp = members[i].split('');
+                while (sp[0] === ' ') {
+                    sp = sp.splice(1);
+                }
+                firstLet = sp[0];
+                arr[x] = firstLet.toUpperCase();
+                x++;
+            }
+        }
+        arr = arr.sort();
+        let res = '';
+        for (let i = 0; i < arr.length; i++) {
+            res += arr[i];
+        }
+        return res;
+    }
 };
